08d0719fd1dfbf55e05ad7b27c234e5bf1406042
captainsza <captainempire786@gmail.com>
2025-03-31 03:55:08 +0530

    Update Open Graph image URL for improved sharing experience

--- app/head.tsx
+++ app/head.tsx
@@ -9,7 +9,7 @@ export default function Head() {
       <meta property="og:title" content="Eid Special - Words of Peace" />
       <meta property="og:description" content="Celebrate Eid with peaceful and inspirational verses from the Quran" />
       <meta property="og:type" content="website" />
-      <meta property="og:image" content="/eid-banner.png" />
+      <meta property="og:image" content="https://empiresphere.vercel.app/api/files/67e9c4acdbc65f5b96d860cc/view?apiKey=emp_cbb0f91b-ae83-4fdc-a4e9-e1deb39ed9cc" />
       <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
